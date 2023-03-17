4d20e474149c1f3317ea7e3636efb406f7288766
AbhinavGoyal2002 <111116579+AbhinavGoyal2002@users.noreply.github.com>
2023-03-17 20:06:51 +0530

    Update model_ae.py

--- main/model/model_ae.py
+++ main/model/model_ae.py
@@ -68,7 +68,7 @@ class Model(ABC):
    def optimizer(self):
        """Optimizer to be used by the model."""
        if self._optimizer is None:
-           self._optimizer = tf.train.GradientDescentOptimizer(learning_rate=self.lr)
+           self._optimizer = tf.optimizers.SGD(learning_rate=self.lr)
 
 
        return self._optimizer
