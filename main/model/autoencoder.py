fcf6ddf9f5cb42e8c78ae712151ba578244b64f1
AbhinavGoyal2002 <111116579+AbhinavGoyal2002@users.noreply.github.com>
2023-03-17 23:29:14 +0530

    Update autoencoder.py

--- main/model/autoencoder.py
+++ main/model/autoencoder.py
@@ -46,9 +46,7 @@ class AutoEncoderModel(Model):
        loss = tf.reduce_mean(tf.square(pred - features))
       
        train_op = self.optimizer.minimize(
-                   loss = loss,
-                   var_list=None,
-                   tape = None)
+                   var_list=None)
 
 
 
